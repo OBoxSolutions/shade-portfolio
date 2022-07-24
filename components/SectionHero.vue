8524b3bf5a15bbdee23c1f96519b64f717bdbfcd
Jose Garcia <jogarcia@tuta.io>
2022-07-24 09:18:10 -0400

    Position navbar absolute

--- components/SectionHero.vue
+++ components/SectionHero.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="section-hero">
-    <section-hero-navbar class="mx-auto mt-2"></section-hero-navbar>
+    <div class="navbar-wrapper mx-auto">
+      <section-hero-navbar></section-hero-navbar>
+    </div>
     <div class="resume"></div>
     <div class="sunset-picture"></div>
   </div>
@@ -14,7 +16,13 @@ export default {
 
 <style scoped>
 .section-hero {
+  position: relative;
   height: 100vh;
   display: flex;
+}
+
+.navbar-wrapper {
+  position: relative;
+  top: 3em;
 }
 </style>
