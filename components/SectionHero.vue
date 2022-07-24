7731183a61e7ceadd68571efa4fa211fef4d4ced
Jose Garcia <jogarcia@tuta.io>
2022-07-24 08:57:09 -0400

    Make SectionHero component

---
+++ components/SectionHero.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="section-hero">
+    <section-hero-navbar class="mx-auto mt-2"></section-hero-navbar>
+    <div class="resume"></div>
+    <div class="sunset-picture"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SectionHero',
+}
+</script>
+
+<style scoped>
+.section-hero {
+  height: 100vh;
+  display: flex;
+}
+</style>
