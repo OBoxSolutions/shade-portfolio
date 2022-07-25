c153a377d1a3438cc874db0a1390c12d034e3657
Jose Garcia <jogarcia@tuta.io>
2022-07-25 09:42:14 -0400

    Set a background color for the other sections

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <section-hero></section-hero>
+    <div class="content-sections"></div>
   </div>
 </template>
 
@@ -9,3 +10,9 @@ export default {
   name: 'IndexPage',
 }
 </script>
+
+<style scoped>
+.content-sections {
+  background-color: #d6932f;
+}
+</style>
