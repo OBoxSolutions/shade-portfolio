8419958c2d68e0a5ac7fec42bb0e997476213b03
Jose Garcia <jogarcia@tuta.io>
2022-07-25 09:54:32 -0400

    Create basic footer

---
+++ components/AppFooter.vue
@@ -0,0 +1,77 @@
+<template>
+  <footer class="footer">
+    <div class="section-change-marker"></div>
+    <ul class="nav-bar">
+      <li
+        v-for="(link, index) in links"
+        :key="`navbar-${index}`"
+        class="nav-bar__link"
+      >
+        {{ link.text }}
+      </li>
+      <div class="location-line"></div>
+    </ul>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter',
+  data() {
+    return {
+      links: [
+        { text: 'About us', url: '' },
+        { text: 'Portfolio', url: '' },
+        { text: 'Contacts', url: '' },
+        { text: 'Get started', url: '' },
+        { text: "We're hiring", url: '' },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.footer {
+  position: relative;
+}
+
+.nav-bar {
+  display: flex;
+  list-style: none;
+  background-color: #5a55f8;
+  color: white;
+  width: fit-content;
+  padding: 0.4em 0.2em;
+  text-transform: uppercase;
+  position: relative;
+  border: 1px solid #222940;
+  width: 1200px;
+
+  .nav-bar__link {
+    display: flex;
+    justify-content: center;
+    flex-grow: 1;
+    border: solid #222940;
+    border-width: 0 2px;
+
+    &:first-child {
+      border: solid #222940;
+      border-width: 0 2px 0 4px;
+    }
+    &:last-of-type {
+      border: solid #222940;
+      border-width: 0 4px 0 2px;
+    }
+  }
+}
+
+.section-change-marker {
+  background-color: #15aaaa;
+  width: 100%;
+  height: 200px;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+</style>
