72f4527d4d1a7a8d46d3f6db414dfa0ddcdffae6
Jose Garcia <jogarcia@tuta.io>
2022-07-28 09:52:44 -0400

    Add an angle of inclination

--- components/SectionAboutUs.vue
+++ components/SectionAboutUs.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="section-head">
+    <div class="section-head mb-8">
       <h2 class="first-header m-0">About us</h2>
       <h2 class="second-header m-0">Why we're here</h2>
     </div>
@@ -13,10 +13,13 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+$angle-of-inclination: 2deg;
+
 .section-head {
   display: flex;
   width: 100%;
+  perspective: 200px;
 }
 
 .first-header,
@@ -25,6 +28,7 @@ export default {
   padding: 20px 0px;
   text-align: center;
   border: 3px solid black;
+  z-index: 100;
 }
 
 .first-header {
@@ -32,10 +36,14 @@ export default {
   text-transform: uppercase;
   font-size: 2rem;
   border-left: 0;
+  transform: skewY(-$angle-of-inclination);
+  perspective-origin: right;
 }
 
 .second-header {
   background-color: #d43737;
   border-right: 0;
+  transform: skewY($angle-of-inclination);
+  perspective-origin: left;
 }
 </style>
