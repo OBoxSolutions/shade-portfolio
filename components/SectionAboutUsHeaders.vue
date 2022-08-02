15adad1ee4cb75b7c02478b4d1957cd2a8f42949
Jose Garcia <jogarcia@tuta.io>
2022-08-01 21:08:09 -0400

    Move out headers

---
+++ components/SectionAboutUsHeaders.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="section-head-cube">
+    <div class="first-header-cube">
+      <div class="first-header__top-side"></div>
+      <h2 class="m-0">About us</h2>
+    </div>
+    <div class="second-header-cube">
+      <div class="second-header__top-side"></div>
+      <h2 class="px-5 m-0">Why we're here</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SectionAboutUsHeaders',
+}
+</script>
+
+<style scoped lang="scss">
+$angle-of-inclination: 2deg;
+$side-bottom-height: 40px;
+$perspective: 200px;
+$headers-rotation-x-degrees: 2deg;
+
+.section-head-cube {
+  display: flex;
+  width: 100%;
+  position: relative;
+  z-index: 100;
+  transform-style: preserve-3d;
+  perspective: $perspective;
+  transform: rotateX(-24deg);
+  perspective-origin: top;
+
+  .first-header-cube,
+  .second-header-cube {
+    width: 100%;
+    padding: 20px 0px;
+    text-align: center;
+    border: 3px solid black;
+    z-index: 100;
+    transform-style: preserve-3d;
+    perspective: $perspective;
+  }
+
+  .first-header-cube {
+    background-color: #ef4242;
+    text-transform: uppercase;
+    border-left: 0;
+    transform: rotateY($headers-rotation-x-degrees) translateX(1.5px);
+
+    h2 {
+      font-size: 6.2rem;
+    }
+  }
+
+  .second-header-cube {
+    background-color: #d43737;
+    border-right: 0;
+    text-align: left;
+    transform: rotateY(-($headers-rotation-x-degrees)) translateX(-1.5px);
+
+    h2 {
+      font-size: 3rem;
+    }
+  }
+}
+</style>
