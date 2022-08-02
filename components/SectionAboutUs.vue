427959b66c244470a9b10fd4c7f0f5ee42ff3a6e
Jose Garcia <jogarcia@tuta.io>
2022-08-01 21:02:43 -0400

    Move description out of about us component

--- components/SectionAboutUs.vue
+++ components/SectionAboutUs.vue
@@ -10,25 +10,18 @@
         <h2 class="px-5 m-0">Why we're here</h2>
       </div>
     </div>
-    <div class="description-cube mb-9">
-      <div class="description-front-side">
-        <div class="p-5">
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia
-            doloribus eaque laudantium commodi nisi quibusdam itaque asperiores
-            recusandae repellat corrupti id neque eveniet vitae voluptate earum
-            nostrum, atque sed quas.
-          </p>
-        </div>
-      </div>
-      <div class="description-down-side"></div>
-    </div>
+    <section-about-use-description></section-about-use-description>
   </div>
 </template>
 
 <script>
+import SectionAboutUseDescription from './SectionAboutUseDescription.vue'
+
 export default {
   name: 'SectionAboutUs',
+  components: {
+    SectionAboutUseDescription,
+  },
 }
 </script>
 
@@ -71,9 +64,6 @@ $headers-rotation-x-degrees: 2deg;
     border-left: 0;
     transform: rotateY($headers-rotation-x-degrees) translateX(1.5px);
 
-    .first-header__top-side {
-    }
-
     h2 {
       font-size: 6.2rem;
     }
@@ -88,36 +78,6 @@ $headers-rotation-x-degrees: 2deg;
     h2 {
       font-size: 3rem;
     }
-  }
-}
-
-.description-cube {
-  position: relative;
-  height: 600px;
-  width: 60%;
-  transform-style: preserve-3d;
-  perspective: $perspective;
-  perspective-origin: bottom;
-  position: relative;
-  top: -20px;
-  z-index: 10;
-
-  .description-front-side,
-  .description-down-side {
-    width: 100%;
-    height: 100%;
-    position: absolute;
-  }
-
-  .description-front-side {
-    background-color: #ddd6bf;
-  }
-
-  .description-down-side {
-    height: $side-bottom-height;
-    background-color: #5a55f8;
-    bottom: -(calc($side-bottom-height / 2));
-    transform: rotateX(-20deg) translateY(calc($side-bottom-height / 2));
   }
 }
 </style>
---
+++ components/SectionAboutUseDescription.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="description-cube mb-9">
+    <div class="description-front-side">
+      <div class="p-5">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia
+          doloribus eaque laudantium commodi nisi quibusdam itaque asperiores
+          recusandae repellat corrupti id neque eveniet vitae voluptate earum
+          nostrum, atque sed quas.
+        </p>
+      </div>
+    </div>
+    <div class="description-down-side"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SectionAboutUseDescription',
+}
+</script>
+
+<style scoped lang="scss">
+$angle-of-inclination: 2deg;
+$side-bottom-height: 40px;
+$perspective: 200px;
+$headers-rotation-x-degrees: 2deg;
+
+.description-cube {
+  position: relative;
+  height: 600px;
+  width: 60%;
+  transform-style: preserve-3d;
+  perspective: $perspective;
+  perspective-origin: bottom;
+  position: relative;
+  top: -20px;
+  z-index: 10;
+
+  .description-front-side,
+  .description-down-side {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+  }
+
+  .description-front-side {
+    background-color: #ddd6bf;
+  }
+
+  .description-down-side {
+    height: $side-bottom-height;
+    background-color: #5a55f8;
+    bottom: -(calc($side-bottom-height / 2));
+    transform: rotateX(-20deg) translateY(calc($side-bottom-height / 2));
+  }
+}
+</style>
