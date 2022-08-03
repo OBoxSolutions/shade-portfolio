7651182148a93081dc9aa0042e20ada889013d1a
Jose Garcia <jogarcia@tuta.io>
2022-08-02 20:55:36 -0400

    Add top side of prism

--- components/SectionAboutUsHeaders.vue
+++ components/SectionAboutUsHeaders.vue
@@ -42,6 +42,16 @@ $headers-rotation-x-degrees: 2deg;
     z-index: 100;
     transform-style: preserve-3d;
     perspective: $perspective;
+
+    .first-header__top-side,
+    .second-header__top-side {
+      position: absolute;
+      width: 100%;
+      height: 400px;
+      background: linear-gradient(90deg, #f8981d 7.29%, #fec111 48.61%);
+      top: -200px;
+      transform: rotateX(-165deg) translateY(200px);
+    }
   }
 
   .first-header-cube {
@@ -49,6 +59,11 @@ $headers-rotation-x-degrees: 2deg;
     text-transform: uppercase;
     border-left: 0;
     transform: rotateY($headers-rotation-x-degrees) translateX(1.5px);
+    perspective-origin: -400%;
+
+    .first-header__top-side {
+      border-right: 26px solid black;
+    }
 
     h2 {
       font-size: 6.2rem;
@@ -60,6 +75,11 @@ $headers-rotation-x-degrees: 2deg;
     border-right: 0;
     text-align: left;
     transform: rotateY(-($headers-rotation-x-degrees)) translateX(-1.5px);
+    perspective-origin: 400%;
+
+    .second-header__top-side {
+      border-left: 26px solid black;
+    }
 
     h2 {
       font-size: 3rem;
