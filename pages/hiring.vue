13368fa2dbf0e790512acdca41e80084d3599c18
Elodin <jcassola96@gmail.com>
2022-08-12 20:24:48 -0400

    create hiring page

---
+++ pages/hiring.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <section-hero-navbar></section-hero-navbar>
+  </div>
+</template>
+
+<script>
+import SectionHeroNavbar from '~/components/SectionHeroNavbar.vue'
+export default {
+  name: 'HiringPage',
+  components: { SectionHeroNavbar },
+
+}
+</script>
+
+<style>
+
+</style>
