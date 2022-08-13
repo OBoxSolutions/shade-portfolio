eea96f347e5ad28073c6fd4961dc65c4974df757
Elodin <jcassola96@gmail.com>
2022-08-13 18:17:55 -0400

    make right column of the basic information

--- components/SectionHiringInformationForm.vue
+++ components/SectionHiringInformationForm.vue
@@ -1,20 +1,35 @@
 <template>
   <div class="section-hiring-form">
-    <div class="section-hiring-form__title">
-      <h1>BASIC INFORMATION</h1>
-    </div>
+    <h1>BASIC INFORMATION</h1>
     <form class="section-form">
       <div class="section-form__column-left">
         <label for="full-name">Full name:</label><br>
-        <input class="full-name" type="text" name="full-name"><br>
+        <input id="full-name" type="text" name="full-name"><br>
         <label for="email-address">Email address:</label><br>
-        <input class="email-address" type="email" name="email-address"><br>
+        <input id="email-address" type="email" name="email-address"><br>
         <label for="country">Country in which you are based:</label><br>
-        <input class="country" type="text" name="country"><br>
+        <input id="country" type="text" name="country"><br>
       </div>
       <div class="section-form__column-right">
         <label for="applying-for">Applying for:</label><br>
-        <textarea class="applying-for" name="applying-for" cols="30" rows="10"></textarea><br>
+        <div class="applying-for">
+          <div>
+            <input id="full-stack" type="radio" name="applying-for" checked value="Full-Stack Web Developer">
+            <label for="full-stack">Full-Stack Web Developer</label>
+          </div>
+          <div>
+            <input id="general-manager" type="radio" name="applying-for" value="General Manager">
+            <label for="general-manager">General Manager</label>
+          </div>
+          <div>
+            <input id="seo-marketing" type="radio" name="applying-for" value="SEO and Marketing Expert">
+            <label for="seo-marketing">SEO and Marketing Expert</label>
+          </div>
+          <div>
+            <input id="vice-president" type="radio" name="applying-for" value="Vice President">
+            <label for="vice-president">Vice President</label>
+          </div>
+        </div>
         <label for="birthdate">Date of Birth:</label><br>
         <fieldset class="birthdate" name="birthdate">
           <input class="birthdate-input year" type="text" name="birthdate-year"> -
@@ -37,34 +52,92 @@ export default {
   border-top: 2px solid #000000;
   background-color: #D43737;
 
-  .section-hiring-form__title{
-    width: 70%;
-    margin-left: auto;
-
-    h1{
-      text-align: right;
-      padding-right: 2rem;
-      font-family: 'Press Start 2P', cursive;
-      font-style: normal;
-      font-size: 3.75rem;
-      font-weight: 400;
-      line-height: 60px;
-      color: #FFFFFF;
-    }
+  h1{
+    text-align: right;
+    padding-right: 2rem;
+    font-size: 3.75rem;
+    font-weight: 400;
+    line-height: 60px;
+    color: #FFFFFF;
   }
-
 
   .section-form{
     width: 100%;
     display: flex;
 
+    label{
+      font-size: 20px;
+      line-height: 20px;
+      color: #FFFFFF;
+    }
+
+    input{
+      background: #D43737;
+      border: 2px solid #000000;
+      outline: none;
+    }
+
     .section-form__column-left{
       width: 44.5%;
 
+      input{
+        height: 1.5rem;
+      }
     }
 
     .section-form__column-right{
       width: 55.5%;
+
+      .applying-for{
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        margin: .5rem 0 2rem;
+        width: 45%;
+        padding: 1rem 2rem;
+        background: #8A8A8A;
+        border: 2px solid #000000;
+
+        div{
+          display: flex;
+          gap: 1em;
+        }
+
+        input[type='radio'] {
+          appearance: none;
+          background-color: #fff;
+          margin: 0;
+          width: 20px;
+          height: 20px;
+          border: 2px solid black;
+          border-radius: 50%;
+
+          display: grid;
+          place-content: center;
+
+          &::before {
+            content: "";
+            width: 16px;
+            height: 16px;
+            border-radius: 50%;
+            transform: scale(0);
+            // transition: 120ms transform ease-in-out;
+            box-shadow: inset 1em 1em #FFAC06;
+          }
+          &:checked::before {
+            transform: scale(1);
+          }
+        }
+
+        label{
+          margin-top: 3px;
+          font-style: normal;
+          font-weight: 400;
+          font-size: 15px;
+          line-height: 15px;
+          color: #FFFFFF;
+        }
+      }
 
       .birthdate{
         border: none;
@@ -72,6 +145,9 @@ export default {
 
         .birthdate-input{
           width: 4rem;
+          height: 1.8rem;
+          text-align: center;
+          color: #FFFFFF
         }
         .month, .day {
           width: 2rem;
