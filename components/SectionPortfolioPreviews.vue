46cb9138b551029e2c329e6ed4911d71067d1513
Elodin <jcassola96@gmail.com>
2022-08-19 21:45:49 -0400

    make porfolio components

---
+++ components/SectionPortfolioPreviews.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Porfolio Previews works!
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
     <section-hero-navbar></section-hero-navbar>
+    <section-porfolio-header></section-porfolio-header>
+    <section-portfolio-previews></section-portfolio-previews>
   </div>
 </template>
 
 <script>
 import SectionHeroNavbar from '~/components/SectionHeroNavbar.vue'
+import SectionPorfolioHeader from '~/components/SectionPorfolioHeader.vue'
+import SectionPortfolioPreviews from '~/components/SectionPortfolioPreviews.vue'
 export default {
-  components: { SectionHeroNavbar },
+  components: { SectionHeroNavbar, SectionPorfolioHeader, SectionPortfolioPreviews },
 
 }
 </script>
