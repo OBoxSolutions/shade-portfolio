0a3d60dc4c6237cdb40bf9431ae07a09bd591af3
Elodin <jcassola96@gmail.com>
2022-08-19 21:40:15 -0400

    create portfolio page

---
+++ pages/portfolio.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <section-hero-navbar></section-hero-navbar>
+  </div>
+</template>
+
+<script>
+import SectionHeroNavbar from '~/components/SectionHeroNavbar.vue'
+export default {
+  components: { SectionHeroNavbar },
+
+}
+</script>
+
+<style>
+
+</style>
