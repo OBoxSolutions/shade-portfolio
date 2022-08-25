83eaac12a7e5f21566ebdd4c37751f2a145b67a5
Jose Garcia <jogarcia@tuta.io>
2022-08-25 10:11:15 -0400

    Create Partnership subsection

---
+++ components/SectionAboutUsPartnership.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="section-partnership">
+    <div class="square">
+      <h1>Partnership</h1>
+      <p>
+        WebDo is very grateful for every new quality connection it gets to make
+        while on this journey, that's why we take communication with clients
+        very seriously. Our colleagues will always be open to your ideas and
+        give you the time necessary to build an idea about your product that you
+        and we will be both proud of. Our website creators will always keep a
+        friendly and suitable environment because that is when the best ideas
+        are born. I truly hope you get to experience it for yourself.
+      </p>
+    </div>
+    <div class="image">
+      <img
+        src="/pixelated-handshake.png"
+        alt="Pixelated image describing a handshake"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SectionAboutUsPartnership',
+}
+</script>
+
+<style scoped>
+.section-partnership {
+  display: flex;
+}
+</style>
