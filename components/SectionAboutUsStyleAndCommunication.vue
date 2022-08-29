2d7c2c80039ede69e3f552ced88d19b630ebfe6d
Jose Garcia <jogarcia@tuta.io>
2022-08-29 09:36:19 -0400

    Add connector lines

--- components/SectionAboutUsStyleAndCommunication.vue
+++ components/SectionAboutUsStyleAndCommunication.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="section-partnership">
+    <div class="connector-line connector-line-left"></div>
     <div class="cube-wrapper left">
       <empty-cube class="cube" perspective-origin="500px"></empty-cube>
     </div>
+    <div class="connector-line connector-line-middle"></div>
     <div class="cube-wrapper right">
       <empty-cube class="cube" perspective-origin="500px"></empty-cube>
     </div>
+    <div class="connector-line connector-line-right"></div>
   </div>
 </template>
 
@@ -44,5 +47,32 @@ export default {}
 .cube-wrapper.right {
   position: absolute;
   right: 40px;
+}
+
+.connector-line {
+  $connector-line-height: 120px;
+
+  height: $connector-line-height;
+  position: absolute;
+  top: calc(50% - calc($connector-line-height / 2));
+  background-color: #d9d9d9;
+  z-index: 10;
+}
+
+.connector-line-left {
+  width: 33px;
+  left: 0;
+}
+
+.connector-line-middle {
+  $width: 674px;
+
+  width: $width;
+  left: calc(50% - $width / 2);
+}
+
+.connector-line-right {
+  width: 33px;
+  right: 0;
 }
 </style>
