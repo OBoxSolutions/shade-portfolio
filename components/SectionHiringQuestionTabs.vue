5fd335047e49bad0be4a247a3b1e50e688c65c61
Elodin <jcassola96@gmail.com>
2022-09-07 17:50:49 -0400

    joint textarea component to father component

--- components/SectionHiringQuestionTabs.vue
+++ components/SectionHiringQuestionTabs.vue
@@ -23,7 +23,11 @@
         :key="`answer-${index}`"
         class="answer">
           <!-- <textarea :id="`input-${index}`" placeholder="Type your answer here..." class="answer-input"></textarea> -->
-          <textarea-input-answer />
+          <!-- <textarea-input-answer /> -->
+          <div class="textarea-container">
+            <p><textarea id='lineCounter' ref="lineCounter" wrap='off' readonly>1</textarea>
+            <textarea id='codeEditor' ref="codeEditor" wrap="off" placeholder="Type your answer here..." @scroll="scroll" @keydown="inputTab" @input="handlerCounter"></textarea></p>
+          </div>
           <div class="file-picker-container">
             <label for="file-picker" class="file-picker">{{ file !== null ? file.name : 'Attach some file to your answer' }}</label>
             <input id="file-picker" type="file" accept="image/*,video/*" @change="loadFile($event)">
@@ -37,10 +41,10 @@
 <script>
 import { mapGetters } from "vuex"
 import { getStorage, ref, uploadBytes } from 'firebase/storage'
-import TextareaInputAnswer from './TextareaInputAnswer.vue';
+// import TextareaInputAnswer from './TextareaInputAnswer.vue';
 
 export default {
-  components: { TextareaInputAnswer },
+  // components: { TextareaInputAnswer },
   data() {
     return {
       questions: [
@@ -62,6 +66,33 @@ export default {
     ...mapGetters(['getUserName']),
   },
   methods: {
+    scroll(){
+      this.$refs.lineCounter.scrollTop = this.$refs.codeEditor.scrollTop;
+      this.$refs.lineCounter.scrollLeft = this.$refs.codeEditor.scrollLeft;
+    },
+    inputTab(e){
+      const { keyCode } = e;
+      const { value, selectionStart, selectionEnd } = this.$refs.codeEditor;
+      if (keyCode === 9) {  // TAB = 9
+        e.preventDefault()
+        this.$refs.codeEditor.value = value.slice(0, selectionStart) + '\t' + value.slice(selectionEnd);
+        this.$refs.codeEditor.setSelectionRange(selectionStart+2, selectionStart+2)
+      }
+    },
+    line_counter() {
+      const lineCount = this.$refs.codeEditor.value.split('\n').length
+      const outarr = [];
+      if (this.lineCountCache !== lineCount) {
+          for (let x = 0; x < lineCount; x++) {
+            outarr[x] = (x + 1)
+          }
+          this.$refs.lineCounter.value = outarr.join('\n')
+      }
+      this.lineCountCache = lineCount
+    },
+    handlerCounter(){
+      this.line_counter();
+    },
     changeTab(tabIndex, answerIndex){
       const nonActiveTabs = document.getElementsByClassName("answer");
       for (let i = 0; i < nonActiveTabs.length; i++) {
@@ -173,5 +204,45 @@ export default {
 }
 .isActive {
   background-color: #414E77 !important;
+}
+
+//Textarea styles
+.textarea-container{
+    height: 80%;
+    p{
+      margin: 0;
+      height: 100%;
+    }
+}
+#codeEditor, #lineCounter {
+    width: 100%;
+    margin: 0;
+    padding: 5px 0;
+    resize: none;
+    font-size: 16px;
+    line-height: 1.2;
+    outline: none;
+    border: none;
+    box-sizing: border-box;
+}
+#lineCounter {
+      display: flex;
+      overflow-y: hidden;
+      text-align: right;
+      box-shadow: none;
+      color: #707070;
+      position: absolute;
+      width: 1.5rem;
+      height: 25rem;
+      /* Determine appearance of line counter */
+      background-color: transparent;
+      color:#ffffff;
+}
+#codeEditor {
+      padding-left: calc(1.5rem + 8px);
+      height: 100%;
+      /* Determine appearance of code editor */
+      background-color: transparent;
+      color:#ffffff;
 }
 </style>
