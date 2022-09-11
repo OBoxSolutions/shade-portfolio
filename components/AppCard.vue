49d112849cfdf8b2e6cd8e19946cab4c1d2ff4f9
Jose Garcia <jogarcia@tuta.io>
2022-09-11 12:52:30 -0400

    Add shadow

--- components/AppCard.vue
+++ components/AppCard.vue
@@ -15,5 +15,6 @@ export default {
   padding: 3em;
   background-color: #ddd6bf;
   border: 1.5px solid black;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4), 0 6px 20px 0 rgba(0, 0, 0, 0.4);
 }
 </style>
