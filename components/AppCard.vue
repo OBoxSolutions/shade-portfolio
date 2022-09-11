4abb6446e72e047bc7a888bbb418442a756cd77e
Jose Garcia <jogarcia@tuta.io>
2022-09-11 11:52:09 -0400

    Create basic card component

---
+++ components/AppCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppCard',
+}
+</script>
+
+<style scoped>
+.card {
+  padding: 10px;
+  background-color: #ddd6bf;
+  border: 1.5px solid black;
+}
+</style>
