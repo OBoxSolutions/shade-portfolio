ae4407ba162bb1822e174cc71a48dc0a765bcbf7
Jose Garcia <jogarcia@tuta.io>
2022-09-11 15:22:21 -0400

    Add mixin like programmatic endpoint

--- plugins/breakpoint.js
+++ plugins/breakpoint.js
@@ -2,5 +2,17 @@ import Breakpoints from 'breakpoints-js'
 
 export default (context, inject) => {
   Breakpoints()
+  idSmAndDown(Breakpoints)
   inject('breakpoints', Breakpoints)
+}
+
+function idSmAndDown(breakpoints) {
+  breakpoints.on('xs sm', {
+    enter: () => {
+      breakpoints.idSmAndDown = true
+    },
+    leave: () => {
+      breakpoints.idSmAndDown = false
+    },
+  })
 }
