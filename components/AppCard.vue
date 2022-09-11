6250bb0a0cd459272ded9014616e671268ceeb1b
Jose Garcia <jogarcia@tuta.io>
2022-09-11 12:54:13 -0400

    Increase border width

--- components/AppCard.vue
+++ components/AppCard.vue
@@ -14,7 +14,7 @@ export default {
 .card {
   padding: 3em;
   background-color: #ddd6bf;
-  border: 1.5px solid black;
+  border: 2px solid black;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4), 0 6px 20px 0 rgba(0, 0, 0, 0.4);
 }
 </style>
