34fe2f20461a1141182f419941ab9481b473b6b8
Jose Garcia <jogarcia@tuta.io>
2022-09-17 19:10:46 -0400

    Add vuex plugin

--- store/index.js
+++ store/index.js
@@ -1,8 +1,19 @@
+import { plugin } from '~/plugins/breakpoint'
+
+export const plugins = [plugin]
+
 export const state = () => ({
   userName: '',
+  breakpoints: {
+    isSmAndDown: false,
+  },
   portfolio: [
     { name: 'Unborder', image: '/portfolio/unborder.png', link: '#' },
-    { name: 'Cleaning Services', image: '/portfolio/cleaning-services.png', link: '#' },
+    {
+      name: 'Cleaning Services',
+      image: '/portfolio/cleaning-services.png',
+      link: '#',
+    },
     { name: 'Caboo', image: '/portfolio/cabbo.png', link: '#' },
     { name: 'Flowers', image: '/portfolio/flowers.png', link: '#' },
   ],
@@ -11,7 +22,10 @@ export const state = () => ({
 export const mutations = {
   setUserName: (state, name) => {
     state.userName = name
-  }
+  },
+
+  setBreakpoints: (state, breakpoints) =>
+    (state.breakpoints = { ...state.breakpoints, ...breakpoints }),
 }
 export const getters = {
   getUserName: (state) => {
@@ -19,5 +33,5 @@ export const getters = {
   },
   getPortfolio: (state) => {
     return state.portfolio
-  }
+  },
 }
