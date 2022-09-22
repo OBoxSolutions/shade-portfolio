7e56789de17f96d081c518c7e2fa23d57db44c85
Elodin <jcassola96@gmail.com>
2022-09-22 13:50:00 -0400

    finish top header

--- components/SectionMobileHiringHeader.vue
+++ components/SectionMobileHiringHeader.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="section-header">
     <div class="__top">
-
+      <div class="title-container">
+        <h1>Every Pixel delivered with care.</h1>
+      </div>
     </div>
     <div class="__bottom">
 
@@ -26,6 +28,23 @@ export default {
     height: 50%;
     background: url('/retro-macintosh.png') right no-repeat, url('/hiring-cover-up.png');
     background-size: contain;
+    background-position-x: calc(100% + 50px);
+
+    .title-container{
+      width: 10rem;
+      background: #FFAC06;
+      border-radius: 5px;
+      padding: 1rem;
+      margin: 30px 0 0 35px;
+      h1{
+        margin: 0;
+        width: 100%;
+        font-size: 13px;
+        line-height: 16px;
+        text-align: center;
+        color: #000000;
+      }
+    }
   }
   .__bottom{
     height: 50%;
