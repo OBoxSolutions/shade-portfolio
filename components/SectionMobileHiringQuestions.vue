7fa8c64d9f7125b541ed6e3509a9dc4f2337b438
Elodin <jcassola96@gmail.com>
2022-09-24 18:49:05 -0400

    set mobile questions

--- components/SectionMobileHiringQuestions.vue
+++ components/SectionMobileHiringQuestions.vue
@@ -1,57 +1,73 @@
 <template>
   <div class="section-questions__outside-box">
-    <div class="section-questions__inside-box">
-      <div class="question-container">
-        <div class="question-title">
-          <h1>Question</h1>
-        </div>
-        <textarea-input-answer class="question-answer" />
+    <div
+      v-for="(question, index) in questions"
+      :key="`question-${index}`"
+      class="question-wraper">
+      <div class="question-info">
+        <h1>{{question.text}}</h1>
+        <textarea-input-answer  class="input-answer" @answer="updateAnswer(question, $event)" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapState, mapMutations } from "vuex"
 export default {
+  name: 'SectionMobileHiringQuestions',
+  computed: {
+    ...mapState(['questions']),
+  },
+  mounted(){
+    document.getElementById('lineCounter').style.setProperty('height', '18rem', 'important')
+  },
+  methods: {
+    ...mapMutations(['setAnswerToQuestion', 'setFileToQuestion']),
+    updateAnswer(question, answer){
+      this.setAnswerToQuestion({question, answer})
+    },
+  },
 
 }
 </script>
 
 <style lang="scss" scoped>
   .section-questions__outside-box{
-    background-color: #8A8A8A;
+    background: linear-gradient(to right, #8A8A8A 6.5%,#8A8A8A 6.5%,#DBDBDB 6.5%,#DBDBDB 93%,#8A8A8A 93%);;
     width: 100%;
-    height: 900px;
     margin: auto;
-    position: relative;
+    padding: 4rem 0;
 
-    .section-questions__inside-box{
-      background-color: #DBDBDB;
-      width: 87%;
-      height: 100%;
-      margin: auto;
+    display: flex;
+    flex-direction: column;
+    gap: 6rem;
 
+    .question-wraper{
+      width: 100%;
+      height: 20rem;
+      background-color: #FFFFFF;
+      padding-bottom: 3rem;
+      box-shadow: 1.19239px 1.19239px 1.49048px 0.298097px rgba(0, 0, 0, 0.25);
 
-      // display: flex;
-      // flex-direction: column;
+      .question-info{
+        width: 94%;
+        margin: auto;
+        h1{
+          height: 2rem;
+          margin: 1rem 0;
+          font-family: 'Lucida Console';
+          font-weight: 400;
+          font-size: 18px;
+          line-height: 20px;
+          color: #000000;
+        }
+        .input-answer{
+          background-color: #222940;
+          height: 18rem;
+        }
 
-      // .questions-white-box{
-      //   background-color: #FFFFFF;
-      //   width: 97%;
-      //   height: 78%;
-      //   box-shadow: 2px 2px 1px #8A8A8A;
-      //   margin: 3rem auto 1rem;
-
-      //   display: flex;
-      //   justify-content: center;
-      //   align-items: center;
-
-      //   .questions{
-      //     border: 5px solid black;
-      //     width: 95%;
-      //     height: 90%;
-      //   }
-      // }
+      }
     }
   }
 </style>
