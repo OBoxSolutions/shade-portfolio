17d7fe17eb9f556bcb4af65fbb0b5f205487d154
Jose Garcia <jogarcia@tuta.io>
2022-10-18 13:56:21 -0400

    Use messages handler

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <app-messages-handler ref="messagesHandler"></app-messages-handler>
     <app-nav-bar></app-nav-bar>
     <nuxt />
     <app-footer></app-footer>
@@ -7,11 +8,29 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   data() {
     return {
       width: '',
     }
+  },
+  computed: {
+    ...mapState('app', ['messages']),
+  },
+  watch: {
+    messages(messages) {
+      const messagesLength = messages.length - 1
+      const type = messages[messagesLength].type
+      const text = messages[messagesLength].text
+      this.showMessage({ type, text })
+    },
+  },
+  methods: {
+    showMessage(message) {
+      this.$refs?.messagesHandler.addMessage(message)
+    },
   },
 }
 </script>
