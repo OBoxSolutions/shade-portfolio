4edda576c064275d624559d691e64a603119b6e2
Jose Garcia <jogarcia@tuta.io>
2022-11-04 08:40:18 -0400

    Create basic AppInputError component

---
+++ components/AppInputError.vue
@@ -0,0 +1,20 @@
+<template>
+  <span v-show="error" class="text-xs text-red-500">
+    {{ error }}
+  </span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'AppInputError',
+  props: {
+    error: {
+      type: String,
+      default: '',
+    },
+  },
+})
+</script>
+
+<style></style>
